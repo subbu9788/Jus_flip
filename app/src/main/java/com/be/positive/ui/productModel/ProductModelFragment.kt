13bb1958bd6dfa254eb995f71c7f208c2f309760
Subramani Naga <subramani@kambaa.com>
2020-07-06 13:29:40 +0530

    Test at 06-07-2020

--- app/src/main/java/com/be/positive/ui/productModel/ProductModelFragment.kt
+++ app/src/main/java/com/be/positive/ui/productModel/ProductModelFragment.kt
@@ -15,6 +15,7 @@ import com.be.positive.utils.NonSwipeableViewPager
 
 class ProductModelFragment : BaseFragment() {
 
+
     private lateinit var galleryViewModel: ProductModelViewModel
 
     override fun onCreateView(
